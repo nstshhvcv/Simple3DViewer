d82ca14243186485428b79f30a89fdcc3746c707
nastya <nstshhvcv@mail.ru>
2026-01-17 16:10:21 +0300

    Реализованы Vector2f и Vector3f

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -1,6 +1,5 @@
 package com.cgvsu.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector2f {
     public Vector2f(float x, float y) {
         this.x = x;
@@ -8,4 +7,59 @@ public class Vector2f {
     }
 
     public float x, y;
+
+    public boolean equals(Vector3f other) {
+        final float eps = 1e-7f;
+        return Math.abs(x - other.x) < eps &&
+                Math.abs(y - other.y) < eps;
+    }
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    // сложение векторов
+    public Vector2f add(Vector2f other) {
+        return new Vector2f(this.x + other.x, this.y + other.y);
+    }
+
+    // вычитание векторов
+    public Vector2f subtract(Vector2f other) {
+        return new Vector2f(this.x - other.x, this.y - other.y);
+    }
+
+    // умножение на скаляр
+    public Vector2f multiply(float scalar) {
+        return new Vector2f(this.x * scalar, this.y * scalar);
+    }
+
+    // деление на скаляр
+    public Vector2f divide(float scalar) {
+        if (scalar == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return new Vector2f(this.x / scalar, this.y / scalar);
+    }
+
+    // длина вектора
+    public float length() {
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    // нормализация
+    public Vector2f normalized() {
+        float len = length();
+        if (len == 0) {
+            return new Vector2f(0, 0);
+        }
+        return new Vector2f(x / len, y / len);
+    }
+
+    // скалярное произведение
+    public float dot(Vector2f other) {
+        return this.x * other.x + this.y * other.y;
+    }
 }
--- src/main/java/com/cgvsu/math/Vector3f.java
+++ src/main/java/com/cgvsu/math/Vector3f.java
@@ -1,6 +1,5 @@
 package com.cgvsu.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector3f {
     public Vector3f(float x, float y, float z) {
         this.x = x;
@@ -9,10 +8,72 @@ public class Vector3f {
     }
 
     public boolean equals(Vector3f other) {
-        // todo: желательно, чтобы это была глобальная константа
         final float eps = 1e-7f;
-        return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
+        return Math.abs(x - other.x) < eps &&
+                Math.abs(y - other.y) < eps &&
+                Math.abs(z - other.z) < eps;
     }
 
     public float x, y, z;
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() { return z; }
+
+    // сложение векторов
+    public Vector3f add(Vector3f other) {
+        return new Vector3f(x + other.x, y + other.y, z + other.z);
+    }
+
+    // вычитание векторов
+    public Vector3f subtract(Vector3f other) {
+        return new Vector3f(x - other.x, y - other.y, z - other.z);
+    }
+
+    // умножение на скаляр
+    public Vector3f multiply(float scalar) {
+        return new Vector3f(x * scalar, y * scalar, z * scalar);
+    }
+
+    // деление на скаляр
+    public Vector3f divide(float scalar) {
+        if (scalar == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return new Vector3f(x / scalar, y / scalar, z / scalar);
+    }
+
+    // длина вектора
+    public float length() {
+        return (float) Math.sqrt(x * x + y * y + z * z);
+    }
+
+    // нормализация вектора
+    public Vector3f normalized() {
+        float len = length();
+        if (len == 0) {
+            return new Vector3f(0, 0, 0);
+        }
+        return new Vector3f(x / len, y / len, z / len);
+    }
+
+    // скалярное произведение
+    public float dot(Vector3f other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    // векторное произведение
+    public Vector3f cross(Vector3f other) {
+        return new Vector3f(
+                y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x
+        );
+    }
 }
